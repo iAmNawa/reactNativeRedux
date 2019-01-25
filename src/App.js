fe151cafa74245889088d5b8c57aa61a17833ef5
Paul Borawski <paulborawski14@gmail.com>
2019-01-25 12:54:31 -0800

    Provider helps react and redux communicate with each other

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 const App = () => {
   return (
-    <View><Text>Hello</Text></View>
+    <Provider store={createStore}>
+      <View><Text>Hello</Text></View>
+    </Provider>
   );
 };
 
